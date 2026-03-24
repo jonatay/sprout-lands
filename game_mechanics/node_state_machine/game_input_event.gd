d9a21deeacb6dc7a329a68ab5a47e4f3d03f686b
jonatay <jono.stretch@gmail.com>
2026-03-24 08:23:49 +0200

    power off

--- game_mechanics/node_state_machine/game_input_event.gd
+++ game_mechanics/node_state_machine/game_input_event.gd
@@ -4,4 +4,8 @@ extends Node
 
 
 static func movement_input():
-	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+
+
+static func is_movement_input():
+	return movement_input() != Vector2.ZERO
--- game_mechanics/node_state_machine/node_state_idle.gd
+++ game_mechanics/node_state_machine/node_state_idle.gd
@@ -1,13 +1,25 @@
 extends NodeState
 
+func get_4dir_index(angle: float) -> int:
+	# Normalize angle to 0 - 2*PI
+	var normalized_angle = fposmod(angle, PI * 2)
+	# Divide into 4 quadrants (90 degrees each)
+	var index = round(normalized_angle / (PI / 2))
+	# Rounding can result in 4, which is the same as 0 (Right)
+	return int(index) % 4
+
+# Usage Example:
+# var angle = velocity.angle()
+# var direction = get_4dir_index(angle)
 
 func _physics_process(delta):
 	var anim_name = "idle_"
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = GameInputEvent.movement_input() #Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction == Vector2.ZERO:
 		animated_sprite_2D.play(anim_name+"down")
 		return
 	var angle = direction.angle()
-	var offset = angle / (PI/4)
-	var index = wrapi(int(offset), 0, 8)
+	#var offset = angle / (PI/4)
+	var index = get_4dir_index(angle) #wrapi(int(offset), 0, 8)
+	print("%f %d" % [angle, index])
 	animated_sprite_2D.play(anim_name+animation_cardinals[index])
